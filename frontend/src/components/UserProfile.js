773e7841539a3d14969456e5046bdb1f0e2d6117
Mohab Rageh <95445033+MohabYasserHTW@users.noreply.github.com>
2023-05-15 19:48:17 +0300

    frontend

---
+++ frontend/src/components/UserProfile.js
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react'
+import { AuthContext } from '../context/auth-context'
+import { Navigate } from 'react-router-dom'
+import Questions from './Questions'
+
+function UserProfile() {
+
+  const authContext = useContext(AuthContext)
+  return (
+    <main>
+        {!authContext.isLoggedIn&&<Navigate to={"/"} replace={true}/>}
+        <div className='userInfo'>
+        {authContext.isLoggedIn &&<h2> {authContext.userName}</h2>}
+        {authContext.isLoggedIn &&<h2> {authContext.userType}</h2>}
+        </div>
+        <hr/>
+        <Questions />
+    </main>
+  )
+}
+
+export default UserProfile
